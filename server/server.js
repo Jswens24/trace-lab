d58f07588c33dc62a66bcc69e962844c38c7390b
Jessica Swenson <jessicaswenson@jessicas-mbp-2.lan>
2022-06-23 19:16:13 -0600

    added rollbar stuff

--- server/server.js
+++ server/server.js
@@ -6,6 +6,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+    accessToken: '31704d41622843de9c5d64246d55caaa',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.use(express.static(path.join(__dirname, "../public")));
 
 
